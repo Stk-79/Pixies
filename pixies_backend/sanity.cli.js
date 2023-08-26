ef18783e0e7d769d8fedf77b29d86a2a2d042609
Sarthak Pati <sarthak07.dev@gmail.com>
2023-08-26 14:02:28 +0530

    Initial files for resource

---
+++ pixies_backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'pm2qt7ra',
+    dataset: 'production'
+  }
+})
---
+++ pixies_backend/sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Pixie_hut',
+
+  projectId: 'pm2qt7ra',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
