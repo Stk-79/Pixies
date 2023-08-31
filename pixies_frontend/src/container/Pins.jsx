ca80459ef885ca9f8ab5e25fedeeafc6fe4dde2a
Sarthak Pati <sarthak07.dev@gmail.com>
2023-08-31 13:39:13 +0530

    Navbar,Sidebar completed. Pins section added.

--- pixies_frontend/src/container/Pins.jsx
+++ pixies_frontend/src/container/Pins.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components";
 
-const Pins = () => {
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div>Pins</div>
-  )
-}
+    <div className="ps-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          user={user}
+        />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed></Feed>}></Route>
+          <Route path="/category/:categoryId" element={<Feed></Feed>}></Route>
+          <Route
+            path="/pin-detail/:pinId"
+            element={<PinDetail user={user}></PinDetail>}
+          ></Route>
+          <Route
+            path="/create-pin"
+            element={<CreatePin user={user}></CreatePin>}
+          ></Route>
+          <Route
+            path="/search"
+            element={
+              <Search
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+              ></Search>
+            }
+          ></Route>
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
-export default Pins
+export default Pins;
