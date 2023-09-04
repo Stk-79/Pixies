e0d3c2e285452e09be863ddba929774138dde048
Sarthak Pati <sarthak07.dev@gmail.com>
2023-09-05 01:00:38 +0530

    User's Profile page done.

--- pixies_frontend/src/components/Login.jsx
+++ pixies_frontend/src/components/Login.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import {
   GoogleOAuthProvider,
   GoogleLogin,
-  googleLogout,
 } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
